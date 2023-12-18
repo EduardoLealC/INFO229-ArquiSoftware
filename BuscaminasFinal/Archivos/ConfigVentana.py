9474509003a708ae64ec4a608d195a8493309bb8
Lewis <63936002+Luis-iom@users.noreply.github.com>
2023-12-18 13:43:20 -0300

    Delete BuscaminasFinal directory

--- BuscaminasFinal/Archivos/ConfigVentana.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-def Configuracion_Inicial_Ventana(Ventana, Titulo, Tamaño):
-    Ventana.title(Titulo)  # Titulo
-    Ventana.geometry(Tamaño)  # Tamaño
-    Ventana.resizable(0, 0)  # No se permite editar el tamaño de la ventana
